a6941fa78654a9c2627c3eec23e426db8f34c5c3
Unknown <chosuwai@gmail.com>
2018-08-11 21:58:16 +0630

    Add Main Screen

--- app/src/main/java/com/example/chosuwai/mmnews/MainActivity.java
+++ app/src/main/java/com/example/chosuwai/mmnews/MainActivity.java
@@ -5,9 +5,15 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +32,47 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        TextView btnNewRegister=findViewById(R.id.btn_register_new_account);
+        btnNewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Main Activity", "Tap Register New Account");
+                Toast.makeText(v.getContext(), "Register New Account", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        Button btnLogin=findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Main Activity", "User Tap Login Button");
+
+                EditText etUserPhone=findViewById(R.id.et_user_phone);
+                String userPhone=etUserPhone.getText().toString();
+
+                if(TextUtils.isEmpty(userPhone)){
+                    etUserPhone.setError("Enter your phone here!");
+                    return;
+                }
+
+                EditText etUserPassword=findViewById(R.id.et_user_password);
+                String userPassword=etUserPassword.getText().toString();
+
+                if(TextUtils.isEmpty(userPassword)){
+                    etUserPassword.setError("Enter your password here!");
+                    return;
+                }
+
+                if(TextUtils.equals(userPhone, "09")
+                        && TextUtils.equals(userPassword, "abc")){
+                    Snackbar.make(v, "Login Success", Snackbar.LENGTH_INDEFINITE).show();
+                }else {
+                    Toast.makeText(v.getContext(), "Phone number or password is incorrect.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 
     @Override
